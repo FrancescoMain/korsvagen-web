a3c2bf8d37ac5fd4d97fc395d4a81a69efe9f26e
francescoMain <cesaranofrancescomain@gmail.com>
2025-08-01 12:40:52 +0200

    feat: Enhance ReviewsManager component with improved styling and functionality

--- client/src/pages/ReviewsManagement.tsx
+++ client/src/pages/ReviewsManagement.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+import { Breadcrumb } from "../components/Dashboard/Breadcrumb";
+import ReviewsManager from "../components/ReviewsManager";
+
 export const ReviewsManagement: React.FC = () => {
   return (
     <div>
